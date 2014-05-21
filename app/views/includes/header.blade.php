ac0fcf655363de941ae1bcc78a171dc2f6f361ab
Vishnu B <root@localhost.localdomain>
2014-05-21 18:34:12 +0530

    Initial commit

---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand" href="#">{{ HTML::image('images/logo.png', 'Trackimon ogo', array('class'=>'logo')) }}</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Reports <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Report 1</a></li>
+            <li><a href="#">Report 2</a></li>
+            <li><a href="#">Report 3</a></li>
+            <li><a href="#">Report 4</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">User <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title')
+	<title>Technowell Trackimon | Vehicle Tracking</title>
+@stop
+@section('content')
+	<div id="mapcanvas"></div>
+@stop
